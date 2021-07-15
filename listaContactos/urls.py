ff9d320cbdd353b0fa5ea5d17ba16dee6cb48789
Corina Hancco <chanccov@unsa.edu.pe>
2021-07-15 13:46:18 -0500

    registro el url de mi pagina de inicio

--- listaContactos/urls.py
+++ listaContactos/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from inicio.views import myHomeView
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('',myHomeView,name='Pagina de Inicio'),
     path('admin/', admin.site.urls),
 ]
