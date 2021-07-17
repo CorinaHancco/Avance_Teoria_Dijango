3ff9c3b255ed5d97c23efc4cfbbbf10ee77a5a6a
Corina Hancco <chanccov@unsa.edu.pe>
2021-07-17 18:42:31 -0500

    haciendo el formulario heredando de form

--- personas/forms.py
+++ personas/forms.py
@@ -1,13 +1,9 @@
 from django import forms
 from .models import Persona
 
-class PersonaForm(forms.ModelForm):
-    class Meta:
-        model = Persona
-        fields = [
-            'nombres',
-            'apellidos',
-            'edad',
-            'donador',
-        ]
+class PersonaForm(forms.Form):
+    nombres = forms.CharField()
+    apellidos = forms.CharField()
+    edad = forms.ImageField()
+    donador = forms.BooleanField()
     
