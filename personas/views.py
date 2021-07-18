6dc84d383967ba1e231b229084ace156bb4b5ae4
Corina Hancco <chanccov@unsa.edu.pe>
2021-07-17 20:56:35 -0500

    redireccion de carpertas

--- personas/views.py
+++ personas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Persona
 from .forms import PersonaForm, RawPersonaForm
 
@@ -55,7 +55,7 @@ def personaDeleteView(request,myID):
     if request.method == 'POST':
         print('Lo borro')
         obj.delete()
-    
+        return redirect('../')
     context = {
         'objeto':obj,
     }
