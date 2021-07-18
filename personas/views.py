97f2eb2d42b2e72007d12e27fa23302ae6288c81
Corina Hancco <chanccov@unsa.edu.pe>
2021-07-17 20:09:42 -0500

    recibiendo errores sin data

--- personas/views.py
+++ personas/views.py
@@ -33,7 +33,13 @@ def searchForHelp(request):
     return render(request,'personas/search.html',{})
 
 def personasAnotherCreateView(request):
-    form = RawPersonaForm(request.POST)
+    form = RawPersonaForm()
+    if request.method == 'POST':
+        form = RawPersonaForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+        else:
+            print(form.errors)
     context = {
         'form':form,
     }
