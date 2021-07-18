0079536dcb69483f634475be964f277df759a69f
Corina Hancco <chanccov@unsa.edu.pe>
2021-07-17 20:33:59 -0500

    iniciando datos de instancia

--- personas/views.py
+++ personas/views.py
@@ -12,7 +12,8 @@ def personaTestView(request):
 
 
 def personaCreateView(request):
-    form = PersonaForm(request.POST or None)
+    obj = Persona.objects.get(id=2)
+    form = PersonaForm(request.POST or None, instance=obj)
     if form.is_valid():
         form.save()
         form = PersonaForm() #blanquea los cambios
