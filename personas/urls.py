7cd1fbe234f50ed89e51b09aafea8a7afb73e8d6
Corina Hancco <chanccov@unsa.edu.pe>
2021-07-19 14:52:55 -0500

    eliminar personas

--- personas/urls.py
+++ personas/urls.py
@@ -21,11 +21,11 @@ from .views import (
 app_name = 'personas'
 urlpatterns = [
     #path('persona/',personaTestView,name='otro'),
-    path('add',personaCreateView,name='createPersona'),
+    #path('add',personaCreateView,name='createPersona'),
     #path('search/',searchForHelp,name='buscar'),
     #path('anotherAdd',personasAnotherCreateView,name='OtroAgregarPersonas'),
     #path('<int:myID>/',personasShowObject, name = 'browsing'),
-    path('<int:myID>/delete/',personaDeleteView, name = 'deleting'),
+    #path('<int:myID>/delete/',personaDeleteView, name = 'deleting'),
     #path('',personasListView, name = 'listing'),
 
     #django5
@@ -33,7 +33,7 @@ urlpatterns = [
     path('<int:pk>/',PersonaDetailView.as_view(),name='persona-detail'),
     path('create/',PersonaCreateView.as_view(), name = 'persona-create'),
     path('<int:pk>/update/',PersonaUpdateView.as_view(), name = 'persona-create'),
-    path('<int:pk>/delete/',PersonaDeleteView.as_view(), name = 'persona-delete'),
+    path('<int:pk>/delete/',PersonaDeleteView.as_view(), name = 'persona-confirm-delete'),
 
     path('query/',PersonaQueryView.as_view(), name ='persona-query'),
 
